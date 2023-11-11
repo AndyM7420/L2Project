d59475b4658e03e2e2bff9b0e426e26d021a03df
andym <amejia9149@bths.edu>
2023-11-10 23:51:22 -0500

    finished project

--- script.js
+++ script.js
@@ -9,8 +9,14 @@ function updateInstructions()
 	instrs = "Welcome to the Haunted House! As you navigate your way through the house, you will have to make decisions based on the descriptions of your surroundings. Click the 'get started' button to begin, and remember, choose wisely!";
 	display();
 }
+function display(){
+	instructionsOutput.innerHTML=instrs;
+}
 count=0;
 code=0;
+function myFunction(){
+	alert("Welcome to the hauntedv house!");
+}
 function saw()
 {
 
@@ -43,11 +49,18 @@ document.getElementById("tease").appendChild(tales);
 }
 function prompts(){
 
-	let users=prompt("Please enter code");
-	document.getElementById("demo").innerHTML="Thank you have open the tresure chest using"+code;
+	document.getElementById("demo").innerHTML="Thank you, you have opened the tresure chest and survived with a great deal of riches! You win";
 	
 }
 function bodies(){
-		document.getElementById("numBodies").innerHTML="correct, you now have one step left to escape";
+		document.getElementById("numBodies").innerHTML="correct, click this button to escape!";
+	
+		const nextStep= document.createElement("BUTTON");
+		nextStep.l
+		const name= document.createTextNode("Next step");
+		nextStep.appendChild(name);
+
+		document.body.appendChild(nextStep);
+		
 }
 
